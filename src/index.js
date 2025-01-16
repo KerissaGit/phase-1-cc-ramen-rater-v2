b77a0b162662c6bdd40cda6602a0586e0a312859
KerissaGit <kerissa525@gmail.com>
2025-01-16 13:24:32 -0500

    ramen assignment

--- src/index.js
+++ src/index.js
@@ -1,29 +1,90 @@
-// index.js
+// // index.js
 
-// Callbacks
+
+//Global Variables
+const ramenName = document.querySelector('#ramen-name')
+const ramenRestaurant = document.querySelector('#ramen-restaurant')
+const ramenImg = document.querySelector('#ramen-image')
+const ramenRating = document.querySelector('#ramen-rating')
+const ramenComment = document.querySelector('#ramen-comment')
+const ramenMenu = document.querySelector('#ramen-menu')
+
+
+//Helper functions 
+const fetchRamen = async () => {
+    try {
+        const response = await fetch("http://localhost:3000/ramens")
+        const data = await response.json()
+        data.forEach(ramenObj => {
+          const image = document.createElement('img')
+          image.src = ramenObj.image
+          image.addEventListener('click', () => handleClick(ramenObj))
+          ramenMenu.append(image)
+        });
+    } catch(error){
+        alert(error)
+    }
+};
+
+fetchRamen();
+
+  
 const handleClick = (ramen) => {
-  // Add code
+    ramenName.textContent = ramen.name
+    ramenRestaurant.textContent = ramen.restaurant
+    ramenImg.src = ramen.image
+    ramenRating.textContent = `Rating: ${ramen.rating}/10`
+    ramenComment.textContent = ramen.comment
 };
 
+
+//Attach Listeners
 const addSubmitListener = () => {
-  // Add code
-}
+    const form = document.getElementById('new-ramen')
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
 
-const displayRamens = () => {
-  // Add code
-};
+   
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+      //Trying different code method to attempt solution
+      const name = document.getElementById('new-name').value
+      const restaurant = document.getElementById('new-restaurant').value
+      const image = document.getElementById('new-image').value
+      const rating = document.getElementById('new-rating').value
+      const comment = document.getElementById('new-comment').value
 
-main()
+      const newRamen = { name, restaurant, image, rating, comment }
+      
+      const ramenImage = document.createElement('img')
+      ramenImage.src = newRamen.image
+      ramenImage.alt = newRamen.name
+      ramenImage.addEventListener('click', () => handleClick(newRamen))
+      ramenMenu.append(ramenImage)
+
+      form.reset()
+    }) 
+  };
+
+  addSubmitListener()
+
+//Invoke Logic
+
+// const main = () => {
+//     document.addEventListener('DOMContentLoaded', () => {
+//         fetchRamen();
+//         addSubmitListener();
+//     })
+// }
+
+// main();
+
 
 // Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+// CODE IS BREAKING AT THIS LINE
+//WILL NOT RETAIN THE SUBMIT INFO
+// export {    
+//   displayRamens,
+//   addSubmitListener,
+//   handleClick,
+//   main,
+// };
